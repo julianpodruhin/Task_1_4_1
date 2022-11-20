ed74a982ad6621670604eedc993439d5b9061d98
julianpodruhin <yulianpodruhin@mail.ru>
2022-11-21 01:37:13 +0300

    mmm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double a = 3.6;
+        double b = 4.1;
+        double c = 5.9;
+        double result = a*b*c;
+        System.out.println(result);
     }
 }
